9612fd7e4b599c42e2d73e3ccc9001d39c4ac7f9
cmallen2106 <cmallen2106@gmail.com>
2021-07-22 21:46:37 -0600

    copy and paste Box program from Lab 3

--- lab4Ex1/src/lab4Ex1/Box.java
+++ lab4Ex1/src/lab4Ex1/Box.java
@@ -1,9 +1,35 @@
+//Lab 4 - Exercise 1
+//Christina Sosa
+//Programming Fundamentals, Summer 2021
+
 package lab4Ex1;
 
 public class Box {
+	
+	int size = 0; 
 
 	public static void main(String[] args) {
-
+		Box box = new Box(); 
+		box.setSize(); 
+		box.printBox(); 
 	}
+	
+	//method to set size of printBox method
+	public void setSize() {
+		size = 5; 
+	}
+	
+	//Add a method to this class named printBox with no parameters and void return type, that
+	//prints out to the screen a set of * (star) characters with size rows and size columns.
+	
+	public void printBox() {
+		for (int num = 1; num <= size; num++) {
+			for (int index = 1; index <= size; index++)
+				System.out.println("*"); 
+			System.out.println(); 
+		}
+	}
+	
+		 
 
 }
