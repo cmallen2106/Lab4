7027c0ab0ba5607b8212da2de634a4e81af449b5
cmallen2106 <cmallen2106@gmail.com>
2021-07-26 21:03:27 -0600

    Add header comment

--- lab4Ex2/src/lab4Ex2/Account.java
+++ lab4Ex2/src/lab4Ex2/Account.java
@@ -1,9 +1,12 @@
+//Lab 4 - Exercise 2
+//Christina Sosa
+//Programming Fundamentals, Summer 2021
+
 package lab4Ex2;
 
 public class Account {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 	}
 
