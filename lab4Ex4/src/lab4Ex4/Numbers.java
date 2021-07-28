274d9bfff83d3501ff51e5f7d5b6a0ac31ebdd02
cmallen2106 <cmallen2106@gmail.com>
2021-07-27 18:32:53 -0600

    updated sort and print

--- lab4Ex4/src/lab4Ex4/Numbers.java
+++ lab4Ex4/src/lab4Ex4/Numbers.java
@@ -3,28 +3,69 @@
 //Programming Fundamentals, Summer 2021
 
 package lab4Ex4;
-import java.util.Arrays; 
-import java.util.Scanner; 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class Numbers {
-
+	  
+	public static int nextLargest(int array[], int n) 
+	    { 
+	        int nextLargestElement = -1;
+	        
+	      for (int i = 0; i < array.length; i++)  
+	      {
+	    	  if (array[i] > n) 
+	    	    {
+	    		  nextLargestElement = array[i];
+	    	break;
+	      }
+	      }
+	       return nextLargestElement;
+	       
+	      }
+	       
 	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
 		
-		//Asking user for array size
-		Scanner scan = new Scanner(System.in); 
-		System.out.println("Input size of Array: "); 
-		int arraySize = Integer.parseInt(scan.nextLine()); 
-		int[] group = new int[arraySize]; 
+		int size;
+		int array[];
+		int sortedArray[];
+				
+		//User inputs and stores size of array		
+		System.out.print("Enter the size of the array: ");
+		size = scan.nextInt();
+		array = new int[size];
+		sortedArray = new int[size];
 		
-		//Asking user and loading array elements 
-		for (int index = 0; index < group.length; index++) {
-		System.out.println("Enter array numbers: " + (index+1) + ": "); 
-		group[index] = scan.nextInt(); 
+		//user inputs and stores numbers
+		System.out.print("Enter the numbers: ");
+		
+		for(int i = 0; i < size; i++)
+		{ array[i] = scan.nextInt();
+			sortedArray[i] = array[i];
 		}
 		
-		//close scanner
-				scan.close();
+		//sort and print list of number and the next larger numbers next to it
+		Arrays.sort(sortedArray);
+		int max = Arrays.stream(array).max().getAsInt();
 		
-
-	}
-}
+		for (int i = 0; i < size; i++)
+		{
+			int nextLargestElement = nextLargest(sortedArray, array[i]);
+		if(nextLargestElement != -1)
+		{
+			System.out.println(array[i] + " : " + nextLargestElement);
+		}
+		else
+		{
+			//System.out.println(array[i]);
+			System.out.println(array[i] + ": " + Integer.MAX_VALUE);
+			
+		}
+		
+		}
+	
+	}		
+	
+  }
